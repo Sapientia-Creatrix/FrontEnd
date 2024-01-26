3d1cc28d8d5161eb89ba30880ce20e8d115e7342
as6325400 <as6325400@gmail.com>
2024-01-26 23:47:58 +0800

    description

--- src/components/user/update_profile.vue
+++ src/components/user/update_profile.vue
@@ -28,10 +28,35 @@
         placeholder="Change Your OCCUPATION here"
       />
     </div>
-
+    <div class="join">
+      <div class="w-60 pt-5 text-center shadow">MAIL</div>
+      <textarea
+        class="textarea textarea-bordered"
+        placeholder="Change Your MAIL here"
+      />
+    </div>
+    <div class="join">
+      <div class="w-60 pt-5 text-center shadow">DESCRIPTION</div>
+      <textarea
+        class="textarea textarea-bordered"
+        placeholder="Change Your DESCRIPTION here"
+      />
+    </div>
+    <div class="grid gap-4 grid-cols-2">
+      <label v-for="(item, index) in data" :key="item" class="flex items-center">
+        <input type="checkbox" v-model="selectedItems[index]" :value="item">
+        <span class="ml-2">{{ item }}</span>
+      </label>
+    </div>
     <button class="btn bg-sky-400">SAVE</button>
     <button class="btn bg-rose-400">CANCEL</button>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import data from "@/components/user/data.json";
+import { ref } from "vue";
+
+const selectedItems = ref<Array<boolean>>(Array(data.length).fill(false));
+
+</script>
