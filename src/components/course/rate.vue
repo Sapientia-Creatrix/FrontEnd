cb54fe37f92f35a8d35517be26170cd88235bc50
as6325400 <as6325400@gmail.com>
2024-01-27 09:33:21 +0800

    ratenum use variable

--- src/components/course/rate.vue
+++ src/components/course/rate.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="w-full my-[2rem] text-center font-bold text-[8rem]">
-    4.5 / 5
+    {{rate}} / 5
   </div>
 </template>
 
 <script setup lang="ts">
-
+const rate = 4.5;;
 </script>
