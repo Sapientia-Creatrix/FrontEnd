6f589793014c8044404b82bfdb31e0571ff7db5b
as6325400 <as6325400@gmail.com>
2024-01-27 12:42:13 +0800

    api suggest

--- src/components/main/course_card.vue
+++ src/components/main/course_card.vue
@@ -2,12 +2,13 @@
   <div class="card w-96 bg-base-100 shadow-xl">
     <!-- add header image here -->
     <div class="card-body">
-      <h2 class="card-title">{{ props.data }}</h2>
-      <p>DESCRIPTION</p>
+      <h2 class="card-title">{{ props.data['name'] }}</h2>
+      <p>{{props.data['difficulty']}}</p>
       <div class="card-actions justify-end">
         <!-- replace this with v-for for skills -->
-        <div class="badge badge-outline">SKILL</div>
-        <div class="badge badge-outline">SKILL</div>
+        <div class="badge badge-outline" v-for="item in skill_list" :key="item">
+          {{ item }}
+        </div>
       </div>
     </div>
   </div>
@@ -20,8 +21,22 @@ import { PropType, defineProps } from "vue";
 
 const props = defineProps({
   data: {
-    type: Number as PropType<number>,
+    type: null as unknown as PropType<any>,
     required: true,
   },
 });
+
+function splitString(str: string): string[] {
+  if(str == null || str.includes('@') === false) {
+    return [str];
+  }
+  return str.split('@');
+}
+
+
+
+const skill = props.data['skills'];
+const skill_list = splitString(skill);
+
+
 </script>
--- src/components/main/display.vue
+++ src/components/main/display.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="py-2 ml-[4rem]">
     <div class="grid gap-8 grid-cols-4 sm:grid-cols-2 lg:grid-cols-4">
-      <div class="" v-for="index in 40" :key="index">
-        <Course_card :data="index"></Course_card>
+      <div class="" v-for="(data, index) in datas" :key="index">
+        <a href=/#/course ><Course_card :data="data" @click="click_course(data.id)"></Course_card></a>
       </div>
     </div>
   </div>
@@ -10,4 +10,30 @@
 
 <script setup lang="ts">
 import Course_card from "@/components/main/course_card.vue";
+import {ref, onMounted} from "vue";
+import axios from 'axios';
+
+let datas = ref<Array<any>>([]);
+
+async function suggest_basic() {
+  try {
+    const response = await axios.get('http://45.55.133.103:3000/suggest/basic', {
+      params: {
+        id: 1
+      }
+    });
+    return response.data; // 直接返回数据
+  } catch (error) {
+    console.error('请求错误:', error);
+  }
+}
+
+function click_course(value:Number): void{
+  console.log(value);
+  localStorage.setItem("course_id", value.toString());
+}
+
+onMounted(async() => {
+  datas.value = await suggest_basic();
+})
 </script>
