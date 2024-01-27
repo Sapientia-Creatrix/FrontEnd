24c7add10bb4c18271700fb11c69943150f03cb6
hirokisora <hirokisora@localhost.localdomain>
2024-01-27 11:41:47 +0800

    badge ui

--- src/components/user/user_badge.vue
+++ src/components/user/user_badge.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="flex item-center justify-center">
-    <div class="grid grid-col-1 py-4 gap-y-10 flex item-center">
+  <div class="flex flex-wrap items-center justify-center">
+    <div class="grid grid-cols-2 grid-flow-row py-4 gap-y-10 gap-x-10">
       <div v-for="item in items">
         <div class="card w-96 bg-base-100 shadow-xl">
           <div class="card-body items-center text-center">
-            <h2 class="card-title">Title</h2>
-            <p>DESCRIPTION {{ item.progress }}</p>
+            <h2 class="card-title"> {{ item.title }} </h2>
+            <p>{{ item.desc }}</p>
             <progress class="progress progress-accent w-56" :value="item.progress" max="100"></progress>
           </div>
         </div>
@@ -18,15 +18,17 @@
 import { ref } from "vue";
 
 interface badge {
+  title: string, desc: string,
   name: string, progress: number
 }
 
 const items = ref<badge[]>([
-  { name: "1", progress: 10 },
-  { name: "2", progress: 20 },
-  { name: "3", progress: 30 },
-  { name: "4", progress: 40 },
-  { name: "5", progress: 50 },
+  { title: "Start Learning", desc: "finish one course", name: "1", progress: 10 },
+  { title: "Starter", desc: "finish ten courses", name: "2", progress: 20 },
+  { title: "Junior", desc: "finish hundred courses", name: "3", progress: 30 },
+  { title: "Senior", desc: "finish thousand courses", name: "4", progress: 40 },
+  { title: "Loyalty I", desc: "login 100 days", name: "5", progress: 50 },
+  { title: "Happy birthday", desc: "login in the account create day", name: "5", progress: 50 },
 ])
 
 </script>
